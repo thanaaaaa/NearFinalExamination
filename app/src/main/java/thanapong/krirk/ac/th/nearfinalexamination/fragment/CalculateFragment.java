543a2d348c506fc08aa646a6d340586349d04c01
cyber01 <tnptnpj37@gmail.com>
2018-02-26 20:19:40 -0800

    Finish Exam+10

--- app/src/main/java/thanapong/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
+++ app/src/main/java/thanapong/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import thanapong.krirk.ac.th.nearfinalexamination.MainActivity;
 import thanapong.krirk.ac.th.nearfinalexamination.R;
@@ -45,7 +48,39 @@ public class CalculateFragment extends Fragment{
 
 //        exchange controler
 
+        Button button = getView().findViewById(R.id.btnExchange);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                EditText editText = getView().findViewById(R.id.edtMoney);
+                String moneyString = editText.getText().toString().trim();
+
+
+                if (moneyString.isEmpty()) {
+                    Toast.makeText(getActivity() , "Please Fill USD money" , Toast.LENGTH_SHORT).show();
+
+
+
+                }   else {
+
+
+                    double moneyADouble = Double.parseDouble(moneyString);
+                    double answerADouble = moneyADouble * 33;
+
+                    String answerString = Double.toString(answerADouble);
+
+                    Toast.makeText(getActivity()  , "Your " + moneyString  + "USD. ==> " + answerString + " THB. " ,
+                            Toast.LENGTH_SHORT).show();
+
+                    editText.setText("");
+
+
+                }
+
+
+            }
+        });
 
 
 
